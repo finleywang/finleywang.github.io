496d2ecb7e525df4e1f1004070782d3ccb00ebd1
finleywang <wangfei@ict.ac.cn>
2025-06-04 14:28:12 +0000

    Deploying to gh-pages from @ finleywang/finleywang.github.io@81a6a9aad890fc13c6286946c6ba83574acd3b05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-two-papers-blast-and-merlin-are-accepted-by-kdd-congrats-to-zezhi-and-chengqing",
+          title: 'Two papers, BLAST and Merlin, are accepted by KDD, congrats to Zezhi and...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
